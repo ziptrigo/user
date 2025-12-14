35818afd9e244b3c238233b953db4410a5dd07d1
Joao Coelho <joaonc@hotmail.com>
2025-12-14 00:42:33 -0600

    project implementation

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from accounts.models import (
+    Service,
+    Permission,
+    Role,
+    RolePermission,
+    User,
+    UserServiceAssignment,
+    UserServiceRole,
+    UserServicePermission,
+    UserGlobalRole,
+    UserGlobalPermission,
+)
+
+admin.site.register(Service)
+admin.site.register(Permission)
+admin.site.register(Role)
+admin.site.register(RolePermission)
+admin.site.register(User)
+admin.site.register(UserServiceAssignment)
+admin.site.register(UserServiceRole)
+admin.site.register(UserServicePermission)
+admin.site.register(UserGlobalRole)
+admin.site.register(UserGlobalPermission)
---
+++ accounts/views/role_permission_views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from accounts.models import Permission, Role, Service
+from accounts.serializers import PermissionSerializer, RoleSerializer
+
+
+class ServicePermissionListCreateView(generics.ListCreateAPIView):
+    serializer_class = PermissionSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        service_id = self.kwargs['service_id']
+        return Permission.objects.filter(service_id=service_id)
+
+    def perform_create(self, serializer):
+        service_id = self.kwargs['service_id']
+        service = Service.objects.get(id=service_id)
+        serializer.save(service=service, type=Permission.TYPE_SERVICE)
+
+
+class ServiceRoleListCreateView(generics.ListCreateAPIView):
+    serializer_class = RoleSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        service_id = self.kwargs['service_id']
+        return Role.objects.filter(service_id=service_id)
+
+    def perform_create(self, serializer):
+        service_id = self.kwargs['service_id']
+        service = Service.objects.get(id=service_id)
+        serializer.save(service=service)
